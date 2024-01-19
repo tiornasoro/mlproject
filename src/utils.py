c030ff98f6b1b9f67da8924c052995f4f8a46e22
Tiorna SORO <tiornasoro094@gmail.com>
2024-01-19 16:40:19 +0100

    HyperParameters for trainig

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import pandas as pd
 import dill 
 from src.exception import CustomException
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path,obj):
 
@@ -18,14 +19,22 @@ def save_object(file_path,obj):
         raise CustomException(e,sys)
     
 
-def evaluate_models(x_train,y_train,x_test,y_test,models):
+def evaluate_models(x_train,y_train,x_test,y_test,models,param):
     try:
         report={}
 
         for i in range(len(list(models))):
-            model = list(models.values())[i]
 
+            model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(x_train,y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(x_train,y_train)
+
+            #model.fit(x_train,y_train)
 
             y_train_pred= model.predict(x_train)
             y_test_pred= model.predict(x_test)
