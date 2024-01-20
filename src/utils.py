f7ba819927dd0b218ea0920a18e185741923820f
Tiorna SORO <tiornasoro094@gmail.com>
2024-01-20 14:15:45 +0100

    Prediction Pipeline

--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ import dill
 from src.exception import CustomException
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
+import dill
 
 def save_object(file_path,obj):
 
@@ -45,6 +46,14 @@ def evaluate_models(x_train,y_train,x_test,y_test,models,param):
             report[list(models.keys())[i]] = test_model_score
 
         return report
+
+    except Exception as e:
+        raise CustomException(e,sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path,"rb") as file_obj:
+            return dill.load(file_obj)
 
     except Exception as e:
         raise CustomException(e,sys)
